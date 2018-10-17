04d9d1a22558b7d61462674bdf645c6bb454b136
andycj7 <635069140@qq.com>
2018-10-17 21:53:21 +0800

    五个输入 两个输出 预测模型

--- 第二题/test.m
+++ 第二题/test.m
@@ -1,18 +1,19 @@
 function [test_result] = test(test_data_input,test_data_output,train_result)
-%测试模型的正确性
+%测试模型的正确性 能测试五个输入参数 两个输出参数的模型
 %input
 %test_data_input:测试数据的输入
 %test_data_output:测试数据的输出
-%train_result
+%train_result:A 模型参数 最后一个为常数项 PS_input 归一化方法信息结构体 PS_output 归一化方法信息结构体
 %output
 %test_result:y:模型预测输出 e:预测输出与实际输出的误差
 test_data_input=mapminmax('apply',test_data_input',train_result.PS_input)';
 
 %test_data_output=mapminmax('apply',test_data_output',train_result.PS_output);
 
-test_result.y=test_data_input*(train_result.A(:,1:5))'+train_result.A(:,6);
+test_result.y(:,1)=test_data_input*(train_result.A(1,1:5))'+train_result.A(1,6);
+test_result.y(:,2)=test_data_input*(train_result.A(2,1:5))'+train_result.A(2,6);
 test_result.y=mapminmax('reverse',test_result.y',train_result.PS_output)';
-test_result.e=test_result.y-test_data_output(:,1);
+test_result.e=test_result.y-test_data_output;
 
 end
 
--- 第二题/train.m
+++ 第二题/train.m
@@ -1,5 +1,5 @@
 function [train_result] = train(train_data_input,train_data_output,m,v)
-%函数用于训练模型 只能用于但变量的输出
+%函数用于训练模型 只能用于五个输入变量，两个输出变量
 %input
 %train_data_input:训练集的输入数据
 %train_data_output:训练集的输出数据
@@ -10,14 +10,22 @@ function [train_result] = train(train_data_input,train_data_output,m,v)
 
 [train_data_input,PS1]=mapminmax(train_data_input',0,1);
 train_data_input=train_data_input';
-[train_data_output(:,1),PS2]=mapminmax(train_data_output(:,1)',0,1);
-train_data_output(:,1)=train_data_output(:,1)';
-A=[1,1,1,1,1,1];
+[train_data_output,PS2]=mapminmax(train_data_output',0,1);
+train_data_output=train_data_output';
+A=[1,1,1,1,1,1;
+   1,1,1,1,1,1];
 for i=1:m
-   E=train_data_output(:,1)-(train_data_input*A(:,1:5)'+A(:,6));
-   d_A=-(E'*train_data_input+sum(E))/size(train_data_output,1);
-   A=A-v*[d_A,-sum(E)/size(train_data_output,1)];
-   if(sum(abs(A))<0.0001)
+   temp=train_data_input*A(:,1:5)';
+   temp(:,1)=temp(:,1)+A(1,6);
+   temp(:,2)=temp(:,2)+A(2,6);
+   E=train_data_output-(temp);
+   temp1=E'*train_data_input;
+   temp2=sum(E)';
+   temp1(1,:)=temp1(1,:)+temp2(1,:);
+   temp1(2,:)=temp1(2,:)+temp2(2,:);
+   d_A=-(temp1)/size(train_data_output,1);
+   A=A-v*[d_A,-sum(E)'/size(train_data_output,1)];
+   if(sum(abs(A(1,:)))<0.0001&&sum(abs(A(2,:)))<0.0001)
        break
    end
 end
